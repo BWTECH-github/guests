e4458dde52e91b6443eafc0e6b11310b7e3991c4
GrossLukas <l.gross@bw.tech>
2026-06-25 10:26:23 +0200

    style(mail): owncloud.online design - centered logo header, BW-TECH signature footer

--- templates/mail/invite.php
+++ templates/mail/invite.php
@@ -25,9 +25,11 @@
 <tr><td>
 <table cellspacing="0" cellpadding="0" border="0" width="600px">
 <tr>
-<td bgcolor="<?php p($theme->getMailHeaderColor());?>" width="20px">&nbsp;</td>
-<td bgcolor="<?php p($theme->getMailHeaderColor());?>">
-<img src="<?php p(\OC::$server->getURLGenerator()->getAbsoluteURL(image_path('', 'logo-mail.gif'))); ?>" alt="<?php p($theme->getName()); ?>"/>
+<td colspan="2" style="padding:0;">
+<table cellspacing="0" cellpadding="0" border="0" width="100%">
+<tr><td style="height:4px;line-height:4px;font-size:0;background-color:<?php p($theme->getMailHeaderColor());?>;">&nbsp;</td></tr>
+<tr><td align="center" style="padding:26px 0 14px;background-color:#ffffff;"><img src="<?php p(\OC::$server->getURLGenerator()->getAbsoluteURL(image_path('', 'logo-mail.gif'))); ?>" alt="<?php p($theme->getName()); ?>" style="display:block;margin:0 auto;border:0;max-width:210px;height:auto;"></td></tr>
+</table>
 </td>
 </tr>
 <tr><td colspan="2">&nbsp;</td></tr>
@@ -59,18 +61,14 @@ if ( isset($_['expiration']) ) {
 	p($l->t("The share will expire on %s.", array($_['expiration'])));
 	print_unescaped('<br><br>');
 }
-// TRANSLATORS term at the end of a mail
-p($l->t('Cheers!'));
 ?>
 </td>
 </tr>
 <tr><td colspan="2">&nbsp;</td></tr>
 <tr>
 	<td width="20px">&nbsp;</td>
-	<td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">--<br>
-		<?php p($theme->getName()); ?> -
-		<?php p($theme->getSlogan()); ?>
-		<br><a href="<?php p($theme->getBaseUrl()); ?>"><?php p($theme->getBaseUrl());?></a>
+	<td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">
+		<?php print_unescaped($this->inc('html.mail.footer')); ?>
 	</td>
 </tr>
 <tr>
